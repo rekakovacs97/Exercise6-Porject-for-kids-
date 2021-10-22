ead285d001b2626dcf5830d273781c7e93b048fc
rekakovacs97 <reka.kovacs@nmg.ro>
2021-10-22 16:31:47 +0300

    added vegetables.html, vegetables.css, fruits.html

--- myJS.js
+++ myJS.js
@@ -1,7 +1,11 @@
-document.getElementById("myButton").onclick = function () {
-    location.href = "vegetables.html";
+document.getElementById("veggieButton").onclick = function () {
+    window.location.href = "vegetables.html";
 };
 
-document.getElementById("backButton").onclick = function () {
-    location.href = "index.html";
+document.getElementById("fruitsButton").onclick = function () {
+    window.location.href = "fruits.html";
 };
+
+function goBack() {
+    window.location.href = "index.html";
+  }
