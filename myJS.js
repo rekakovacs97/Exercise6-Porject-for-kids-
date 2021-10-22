001904469cb2bcbc9116821c01d26a720a89824f
rekakovacs97 <reka.kovacs@nmg.ro>
2021-10-22 14:38:25 +0300

    added js for button

---
+++ myJS.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").onclick = function () {
+    location.href = "vegetables.html";
+};
+
+document.getElementById("backButton").onclick = function () {
+    location.href = "index.html";
+};
